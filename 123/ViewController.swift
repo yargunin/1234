26e523efbd1c86e69c90901df80760befed00c3e
yargunin <vip.yargunin@mail.ru>
2023-02-12 19:11:35 +0400

    123

--- 123/ViewController.swift
+++ 123/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var gretingButton: UIButton!
+    
+    @IBOutlet var gretingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // убираем надпись до нажатия кнопки
+        gretingLabel.isHidden.toggle()
+        
+        // скругляем углы у кнопки
+        gretingButton.layer.cornerRadius = 10
     }
 
 
+    @IBAction func gretingButtonTab(_ sender: Any) {
+        
+    // вкл выкл приветсвия
+        gretingLabel.isHidden.toggle()
+        gretingButton.setTitle(
+            gretingLabel.isHidden ? "Показать" :  "Убрать", for: .normal
+        )
+        
+    }
 }
 
